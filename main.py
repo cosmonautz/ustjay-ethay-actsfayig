66404db850374bdcb166340512f0a4d775c2b9d9
Kevin Waldron <kevinmwaldron@gmail.com>
2019-03-09 19:51:51 -0800

    my changes

--- main.py
+++ main.py
@@ -1,14 +1,12 @@
 import os
 
 import requests
-from flask import Flask, send_file, Response
+from flask import Flask, send_file, Response, render_template
 from bs4 import BeautifulSoup
 
 app = Flask(__name__)
 
-
 def get_fact():
-
     response = requests.get("http://unkno.com")
 
     soup = BeautifulSoup(response.content, "html.parser")
@@ -16,11 +14,18 @@ def get_fact():
 
     return facts[0].getText()
 
+def get_pig_latin_link(fact):
+    response = requests.post('https://hidden-journey-62459.herokuapp.com/piglatinize/',
+        data= {'input_text':fact}, allow_redirects=False)
+
+    return response.headers['Location']
 
 @app.route('/')
 def home():
-    return "FILL ME!"
+    fact = get_fact()
+    link = get_pig_latin_link(fact)
 
+    return render_template('link.html', address=link, quote=fact)
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 6787))
